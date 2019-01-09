cf28d7229c80ce434ed95b995c02fa2cf5bb83fd
zhangbingxue <zhangbingxue@qq.com>
2019-01-09 10:42:28 +0800

    ljkhh

--- js/Zbx/zbx-zhichiyufuwu.js
+++ js/Zbx/zbx-zhichiyufuwu.js
@@ -9,4 +9,23 @@ window.onload = function () {
         zbxLi[1].className = 'zbx-li-xuanz'; 
 }
 }
+
+
+// 新浪的显示
+var aaOimage = document.querySelectorAll('.zbx-change .zbx-aaimg');console.log(aaOimage);
+var aaOhover = document.querySelectorAll('.zbx-change .zbx-hover');
+console.log(aaOhover);
+
+// aaOimage[0].onmouseover = function(){
+//     aaOhover[0].style.display = 'block';
+// }
+for(let i = 0; i < aaOimage.length; i++){
+    aaOimage[i].onmouseover = function(){
+        aaOhover[i].style.display = 'block';
+    }
+    aaOhover[i].onmouseout = function(){
+        this.style.display = 'none';
+    }
+    }
 }
+
